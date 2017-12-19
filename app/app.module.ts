5e0c4d11a9678060efdeaead71f70e90feaa1e5c
Talmid Haham <shimon.tabo@gmail.com>
2017-12-19 17:17:21 +0200

    after routing lesson

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,23 +1,54 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
+
+
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
 
 import { EventsAppComponent } from './events-app.component'
 
-import { EventsListComponent } from './events/events-list.component'
+//import { EventsListComponent } from './events/events-list.component'
 
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
+//import { EventThumbnailComponent } from './events/event-thumbnail.component'
 
 import { NavBarComponent } from './nav/navbar.component'
 
-import { EventService } from './events/shared/event.service'
+//import { EventService } from './events/shared/event.service'
 
 import { ToastrService } from './common/toastr.service' 
 
+import { appRoutes } from './routes'
+import { Error404Component } from './errors/404.component'
+
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [EventsAppComponent,EventsListComponent,EventThumbnailComponent,NavBarComponent],
+  imports: [BrowserModule,  RouterModule.forRoot(appRoutes)],
+  declarations: [EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    CreateEventComponent,
+    EventDetailsComponent,
+    Error404Component],
   bootstrap: [EventsAppComponent],
-  providers:[EventService,ToastrService]
+  providers:[EventService,ToastrService,EventRouteActivator,EventListResolver,
+    { 
+      provide: 'canDeactivateCreateEvent', 
+      useValue: checkDirtyState 
+    }]
 })
 export class AppModule {}
+
+function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
+}
